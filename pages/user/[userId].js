c203489168745cdd119bfc8f3e6306dfb511b368
Aidan Ryan <aidoryan@gmail.com>
2021-11-26 16:43:58 +0100

    commit

--- pages/user/[userId].js
+++
@@ -1,6 +0,0 @@
-import { css } from '@emotion/react';
-import Cookies from 'js-cookie';
-import { useState } from 'react';
-import Header from '../../components/Header';
-
-<div>Personal user page of {props.user.name || props.user.username}</div>;
